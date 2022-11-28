a097b88c25ddc99056bc1c1294376d94e01623c4
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2022-11-28 02:49:58 +0200

    [+] added scene loader [*] extracted bootstrapping to Initial scene

--- Assets/CodeBase/Infrastructure/States/BootstrapState.cs
+++ Assets/CodeBase/Infrastructure/States/BootstrapState.cs
@@ -5,21 +5,30 @@ namespace CodeBase.Infrastructure.States
 {
     public class BootstrapState : IState
     {
+        private const string Initial = "Initial";
         private readonly GameStateMachine _stateMachine;
+        private readonly SceneLoader _sceneLoader;
 
-        public BootstrapState(GameStateMachine stateMachine)
+        public BootstrapState(GameStateMachine stateMachine, SceneLoader sceneLoader)
         {
             _stateMachine = stateMachine;
+            _sceneLoader = sceneLoader;
         }
 
         public void Enter()
         {
             RegisterServices();
+            _sceneLoader.Load(Initial, EnterLoadLevel);
         }
 
         private void RegisterServices()
         {
             Game.InputService = RegisterInputService();
+        }
+
+        private void EnterLoadLevel()
+        {
+            
         }
 
         public void Exit()
