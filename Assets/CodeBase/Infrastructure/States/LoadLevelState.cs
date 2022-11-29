cf7badaa68bad2575e7a0639746f6d6cd72ddd43
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2022-11-29 02:32:06 +0200

    [-] removed Hero from Main scene [-] removed Hud from Main Scene [+} instantiating Hero in LoadLevelState [+] instantiating Hud in LoadLevelState

--- Assets/CodeBase/Infrastructure/States/LoadLevelState.cs
+++ Assets/CodeBase/Infrastructure/States/LoadLevelState.cs
@@ -1,7 +1,11 @@
-﻿namespace CodeBase.Infrastructure.States
+﻿using CodeBase.CameraLogic;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure.States
 {
     public class LoadLevelState : IPayloadedState<string>
     {
+        private const string InitialPoint = "InitialPoint";
         private readonly GameStateMachine _stateMachine;
         private readonly SceneLoader _sceneLoader;
 
@@ -13,7 +17,33 @@
 
         public void Enter(string sceneName)
         {
-            _sceneLoader.Load(sceneName);
+            _sceneLoader.Load(sceneName, OnLoaded);
+        }
+
+        private void OnLoaded()
+        {
+            GameObject initialPoint = GameObject.FindWithTag(InitialPoint);
+            
+            GameObject hero = Instantiate("Hero/Hero", initialPoint.transform.position);
+            Instantiate("Hud/Hud");
+            CameraFollow(hero);
+        }
+
+        private static void CameraFollow(GameObject hero)
+        {
+            Camera.main.GetComponent<CameraFollow>().Follow(hero);
+        }
+        
+        private GameObject Instantiate(string path)
+        {
+            var prefab = Resources.Load<GameObject>(path);
+            return Object.Instantiate(prefab);
+        }
+        
+        private GameObject Instantiate(string path, Vector3 at)
+        {
+            var prefab = Resources.Load<GameObject>(path);
+            return Object.Instantiate(prefab, at, Quaternion.identity);
         }
 
         public void Exit()
