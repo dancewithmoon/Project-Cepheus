39836542aebb4601f82396c280f747f6e3e0255a
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2022-12-05 00:52:31 +0200

    [+] added ability to run from any scene

--- Assets/CodeBase/Infrastructure/GameBootstrapper.cs
+++ Assets/CodeBase/Infrastructure/GameBootstrapper.cs
@@ -6,15 +6,29 @@ namespace CodeBase.Infrastructure
 {
     public class GameBootstrapper : MonoBehaviour, ICoroutineRunner
     {
+        private static GameBootstrapper _instance;
+        
         [SerializeField] private LoadingCurtain _loadingCurtain;
         private Game _game;
 
         private void Awake()
         {
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = this;
+            DontDestroyOnLoad(this);
+            
+            InitGame();
+        }
+
+        private void InitGame()
+        {
             _game = new Game(this, _loadingCurtain);
             _game.StateMachine.Enter<BootstrapState>();
-
-            DontDestroyOnLoad(this);
         }
     }
 }
