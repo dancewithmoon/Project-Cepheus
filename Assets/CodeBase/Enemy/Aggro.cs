eea856be6652f8dd98061d6b9e3c5c98f6f9d5ad
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2022-12-10 17:21:23 +0200

    [+] added trigger observer component [+] added aggro behaviour

---
+++ Assets/CodeBase/Enemy/Aggro.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Enemy
+{
+    public class Aggro : MonoBehaviour
+    {
+        [SerializeField] private TriggerObserver _triggerObserver;
+        [SerializeField] private AgentMoveToHero _follow;
+
+        private void Start()
+        {
+            _triggerObserver.TriggerEnter += TriggerEnter;
+            _triggerObserver.TriggerExit += TriggerExit;
+
+            StopFollow();
+        }
+
+        private void TriggerEnter(Collider obj) => StartFollow();
+        private void TriggerExit(Collider obj) => StopFollow();
+
+        private void StartFollow()
+        {
+            _follow.enabled = true;
+        }
+
+        private void StopFollow()
+        {
+            _follow.enabled = false;
+        }
+
+        private void OnDestroy()
+        {
+            _triggerObserver.TriggerEnter -= TriggerEnter;
+            _triggerObserver.TriggerExit -= TriggerExit;
+        }
+    }
+}
