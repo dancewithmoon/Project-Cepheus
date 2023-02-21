29e1ed53cc969921638ce49e7d7e4a1835382e93
dancewithmoon <ihor.reshetnykov.workonly@gmail.com>
2023-02-21 19:58:47 +0200

    [*] extracted component which handles position and saves/loads it

--- Assets/CodeBase/Hero/HeroMove.cs
+++ Assets/CodeBase/Hero/HeroMove.cs
@@ -1,30 +1,24 @@
-﻿using CodeBase.Data;
-using CodeBase.Infrastructure.Services.PersistentProgress;
+﻿using CodeBase.Infrastructure.Services.PersistentProgress;
 using CodeBase.Services.UserInput;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.SceneManagement;
 using Zenject;
 
 namespace CodeBase.Hero
 {
     [RequireComponent(typeof(CharacterController))]
-    public class HeroMove : MonoBehaviour, ISavedProgress
+    public class HeroMove : MonoBehaviour
     {
         [SerializeField] private float _movementSpeed = 4.0f;
         [SerializeField] private CharacterController _characterController;
         private IInputService _inputService;
-        private IPersistentProgressService _progressService;
         private Camera _camera;
         private NavMeshAgent _agent;
 
-        private PositionOnLevel PositionOnLevel => _progressService.Progress.WorldData.PositionOnLevel;
-        
         [Inject]
         public void Construct(IInputService inputService, IPersistentProgressService progressService)
         {
             _inputService = inputService;
-            _progressService = progressService;
             
             _camera = Camera.main;
             _agent = GetComponent<NavMeshAgent>();
@@ -63,31 +57,5 @@ namespace CodeBase.Hero
         {
             _characterController.enabled = true;
         }
-
-        public void LoadProgress()
-        {
-            if (GetCurrentLevelName() != PositionOnLevel.Level)
-                return;
-
-            Vector3Data savedPosition = PositionOnLevel.Position;
-
-            Warp(savedPosition.AsUnityVector());
-        }
-
-        public void UpdateProgress()
-        {
-            PositionOnLevel.Level = GetCurrentLevelName();
-            PositionOnLevel.Position = transform.position.AsVectorData();
-        }
-
-        private void Warp(Vector3 to)
-        {
-            _characterController.enabled = false;
-            transform.position = to.AddY(_characterController.height);
-            _characterController.enabled = true;
-        }
-
-        private string GetCurrentLevelName() => 
-            SceneManager.GetActiveScene().name;
     }
 }
---
+++ Assets/CodeBase/Hero/HeroPositionHandler.cs
@@ -0,0 +1,50 @@
+﻿using CodeBase.Data;
+using CodeBase.Infrastructure.Services.PersistentProgress;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
+
+namespace CodeBase.Hero
+{
+    [RequireComponent(typeof(CharacterController))]
+    public class HeroPositionHandler : MonoBehaviour, ISavedProgress
+    {
+        [SerializeField] private CharacterController _characterController;
+
+        private IPersistentProgressService _progressService;
+
+        private PositionOnLevel PositionOnLevel => _progressService.Progress.WorldData.PositionOnLevel;
+
+        [Inject]
+        private void Construct(IPersistentProgressService progressService)
+        {
+            _progressService = progressService;
+        }
+        
+        public void LoadProgress()
+        {
+            if (GetCurrentLevelName() != PositionOnLevel.Level)
+                return;
+
+            Vector3Data savedPosition = PositionOnLevel.Position;
+
+            Warp(savedPosition.AsUnityVector());
+        }
+
+        public void UpdateProgress()
+        {
+            PositionOnLevel.Level = GetCurrentLevelName();
+            PositionOnLevel.Position = transform.position.AsVectorData();
+        }
+
+        private void Warp(Vector3 to)
+        {
+            _characterController.enabled = false;
+            transform.position = to.AddY(_characterController.height);
+            _characterController.enabled = true;
+        }
+
+        private string GetCurrentLevelName() => 
+            SceneManager.GetActiveScene().name;
+    }
+}
